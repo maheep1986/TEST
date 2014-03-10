5f2e96db44913171e38ca34131c174d98476e074
maheep1986 <maheep.mnc@gmail.com>
2014-03-10 18:34:29 +0530

    No. is 333

---
+++ Contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+	<link href="Site.css" rel="stylesheet" />
+</head>
+
+<body>
+
+	<?php include("Header.php"); ?>
+    
+<div id="main">  
+	<address>
+		<h1>HEAD OFFICE</h1>
+    	<h2> Shekhawat-Complax </h2> <br />
+        Jawahar Nagar,
+        Sri Ganganagar,
+        (Rajasthan)<br />
+        Email ID: <a href="http://www.gmail.com" target="_blank"> maheep.mnc@gmail.com	</a> <br />
+        Mobile No.: 09460265333
+    </address>
+<div align="right">    
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3458.263755778481!2d73.88623885!3d29.91430805000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3917b5199e7730fd%3A0x33ecabe1b434cf48!2sLakme+Salon!5e0!3m2!1sen!2sin!4v1393832947523" width="900" height="450" frameborder="5px" style="border:2"></iframe>
+</div>    
+	<?php include("Footer.php"); ?>
+</div>
+</body>
+</html>
